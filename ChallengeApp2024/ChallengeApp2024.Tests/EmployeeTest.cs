9fbebd7dcab6d848c3647a7722853368e806956c
robmarych <robmarych@gmail.com>
2024-10-30 22:03:06 +0100

    Zadanie 7

---
+++ ChallengeApp2024/ChallengeApp2024.Tests/EmployeeTest.cs
@@ -0,0 +1,36 @@
+namespace ChallengeApp2024.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void EmpoyeesAddGrade()
+        {
+            // arrage
+            Employee employee = new Employee("Robert", "Kowlskie", 52);
+            employee.AddGrade(5);
+            employee.AddGrade(6);
+            
+            // act
+            var result = employee.Result;
+            
+            // asert
+            Assert.AreEqual(11, result);
+        }
+
+        [Test]
+        public void EmpoyeesAddGradeNegativ()
+        {
+            // arrage
+            Employee employee = new Employee("Karol", "Mowakowski", 40);
+            employee.AddGrade(10);
+            employee.AddGrade(2);
+            employee.AddGrade(-12);
+
+            // act
+            var result = employee.Result;
+
+            // asert
+            Assert.AreEqual(0, result);
+        }
+    }
+}
