35f5f441f51ee8ff813f1cac241040b92db9bf32
robmarych <robmarych@gmail.com>
2024-10-31 18:13:34 +0100

    zd7_fix

--- ChallengeApp2024/ChallengeApp2024.Tests/EmployeeTest.cs
+++ ChallengeApp2024/ChallengeApp2024.Tests/EmployeeTest.cs
@@ -5,7 +5,7 @@ namespace ChallengeApp2024.Tests
         [Test]
         public void EmpoyeesAddGrade()
         {
-            // arrenge
+            // arrange
             Employee employee = new Employee("Robert", "Kowlskie", 52);
             employee.AddGrade(5);
             employee.AddGrade(6);
@@ -13,14 +13,14 @@ namespace ChallengeApp2024.Tests
             // act
             var result = employee.Result;
             
-            // asert
+            // assert
             Assert.AreEqual(11, result);
         }
 
         [Test]
         public void EmpoyeesAddGradeNegativ()
         {
-            // arrenge
+            // arrange
             Employee employee = new Employee("Karol", "Mowakowski", 40);
             employee.AddGrade(10);
             employee.AddGrade(2);
@@ -29,7 +29,7 @@ namespace ChallengeApp2024.Tests
             // act
             var result = employee.Result;
 
-            // asert
+            // assert
             Assert.AreEqual(0, result);
         }
     }
